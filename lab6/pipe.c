e20221a98c113674026df641792817300dbcac8a
zagarake <zagarake@student.21-school.ru>
2024-11-22 13:32:18 +0300

    v 0.4

--- lab6/pipe.c
+++ lab6/pipe.c
@@ -6,11 +6,9 @@
 
 int main() {
     int pipefd[2];
-    char buf[1024];
     pid_t pid;
-    time_t now = time(NULL);
-    struct tm *tm_info = localtime(&now);
-    printf("Принимающий процесс: PID: %d, Время: %s\n", getpid(), asctime(tm_info));
+    char buf[1024];
+    struct tm *tm_info;
 
     // Создание pipe
     if (pipe(pipefd) == -1) {
@@ -26,29 +24,26 @@ int main() {
     }
 
     case 0:{  // Дочерний процесс
+        time_t now = time(NULL);
         tm_info = localtime(&now);
-        char local_time[64];
-        strftime(local_time, sizeof(local_time), "%Y-%m-%d %H:%M:%S", tm_info);
         printf("Дочерний процесс: Время: %s\n", asctime(tm_info));
         close(pipefd[1]);
-        printf("Received: %s\n", buf);
         // Чтение данных из pipe
         int len = 0;
-        while (len = read(pipefd[0], buf, sizeof(buf)) != 0) {
-        read(2, buf, len);
+        while ((len = read(pipefd[0], buf, sizeof(buf))) != 0) {
+        write(2, buf, len);
         }
         close(pipefd[0]);
         break;
     } default: {  // Родительский процесс
 
         sleep(5);
+        time_t now = time(NULL);    
         tm_info = localtime(&now);
-        char local_time[64];
-        strftime(local_time, sizeof(local_time), "%Y-%m-%d %H:%M:%S", tm_info);
-        printf("Родительский процесс: Время: %s\n", asctime(tm_info));
+        sprintf(buf,"Родительский процесс: Время: %s\nPID: %d\n", asctime(tm_info), getpid());
         close(pipefd[0]);
         // Пишем данные в pipe
-        write(pipefd[1], buf, strlen(buf) + 1);
+        write(pipefd[1], buf, strlen(buf));
         close(pipefd[1]);
         break;
     }
