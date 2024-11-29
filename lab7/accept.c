ab738a0a2562da357ada6e6bf7e89a8ee279cf3a
zagarake <zagarake@student.21-school.ru>
2024-11-29 12:52:42 +0300

    v 0.2

--- lab7/accept.c
+++ lab7/accept.c
@@ -7,7 +7,6 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 
-#define SHM_KEY 0x1234
 #define SHM_SIZE 1024
 
 typedef struct {
@@ -18,7 +17,7 @@ typedef struct {
 int main() {
     int shmid;
     shared_data *data;
-
+    
     // Подключаемся к существующему сегменту разделяемой памяти
     shmid = shmget(SHM_KEY, SHM_SIZE, 0666);
     if (shmid == -1) {
@@ -38,12 +37,13 @@ int main() {
 
     // Бесконечный цикл чтения данных
     while (1) {
+        sleep(3);
         time_t now = time(NULL);
         struct tm *tm_info = localtime(&now);
         char local_time[64];
         strftime(local_time, sizeof(local_time), "%Y-%m-%d %H:%M:%S", tm_info);
 
-        printf("Принимающий процесс: PID: %d, Время: %s\n", getpid(), local_time);
+        printf("Принимающий процесс: PID: %d, Время: %s ", getpid(), local_time);
         printf("Принято от PID: %d, Время: %s\n", data->pid, data->time_str);
 
         sleep(1);
